c2c714cb156218f95977e2399eea548e5d46ba49
Adan Lopez <adan1001917@gmail.com>
2020-09-11 15:37:22 -0700

    added books.html and routed it

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/books')
+def books():
+    return render_template('books.html')
+
 @app.route('/ar')
 def ar():
     return render_template('AR.html')
