a3186f6cc1ee39cf202abc7b9a25e53f61df262c
Haoran He <hrgutou@gmail.com>
2020-09-12 01:23:01 -0400

    Update home page

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,17 @@ import csv
 app = Flask(__name__)
 app.secret_key="zbc"
 
-def loader(college):
+def loaderNetCost(college):
+    try:
+        data = csv.DictReader(open("net_price.csv"))
+        for row in data:
+            if college == row['Institution Name']:
+                return list(row.items())[7:16]
+    except:
+        pass
+
+
+def loaderAdCost(college):
     try:
         data = csv.DictReader(open("sticker_price.csv"))
         for row in data:
@@ -24,9 +34,9 @@ def index():
 @app.route('/home', methods=['POST','GET'])
 def home():
     college = session.get('college')
-    data = loader(college)
-
-    return render_template('home.html', data=data, college=college, income=session.get('income'))
+    AdCost = loaderAdCost(college)
+    NetCost = loaderAdCost(college)
+    return render_template('home.html', NetCost=NetCost, AdCost=AdCost, college=college, income=session.get('income'))
 
 @app.route('/books')
 def books():
@@ -47,4 +57,6 @@ def reciepe():
 
 if __name__ == "__main__":
 
-    print(type(loader('CUNY City College')[0]))
+    data = loaderNetCost("CUNY City College")
+    for i in data:
+        print(i)
